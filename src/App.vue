8d5515dde9c77c81f3ed337ad1a16cde4ab947dd
Amad Ul Hassan <ah@indigilabs.com>
2024-08-26 19:51:13 +0200

    added the minmaxchart
    added the minmaxchart

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,45 @@
   <div class="container">
     <h1>Time Series Charts</h1>
 
-    <div>
-      <MinMaxAreaChart />
+    <div v-if="chartData">
+      <MinMaxAreaChart :data="chartData" />
     </div>
 
-    <div>
+    <div v-else>
       <p>Loading data, please wait...</p>
     </div>
   </div>
 </template>
+
+<script>
+import MinMaxAreaChart from "./components/MinMaxAreaChart.vue";
+import { fetchData } from "./services/apiService";
+import { transformData } from "./utils/transformData";
+
+export default {
+  name: "App",
+  components: {
+    MinMaxAreaChart,
+  },
+  data() {
+    return {
+      chartData: null,
+    };
+  },
+  async mounted() {
+    const rawData = await fetchData();
+    if (rawData) {
+      this.chartData = transformData(rawData);
+      console.log(this.chartData);
+    }
+  },
+};
+</script>
+
+<style>
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
+}
+</style>
